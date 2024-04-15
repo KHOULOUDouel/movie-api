32e081ea0adbe1ad317282ad6535e46bfb524924
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-04-15 14:41:10 +0200

    Create an Express GET route

--- index.js
+++ index.js
@@ -1,2 +1,67 @@
 const express = require('express');
 const app = express();
+
+const express = require('express');
+const app = express();
+
+let topMovies = [
+  {
+    title: 'Titanic',
+    director: 'James Cameron'
+  },
+  {
+    title: 'Twilight',
+    director: 'Catherine Hardwicke'
+  },
+  {
+    title: 'Remember me',
+    director: 'Allen Coulter'
+  },
+  {
+    title: 'Ghost Ship',
+    director: 'Steve Beck'
+  },
+  {
+    title: 'Schindler\'s List',
+    director: 'Steven Spielberg'
+  },
+  {
+    title: 'The Lord of the Rings',
+    director: 'Peter Jackson'
+  },
+  {
+    title: 'Forrest Gump',
+    director: 'Robert Zemeckis'
+  },
+  {
+    title: 'Inception',
+    director: 'Christopher Nolan'
+  },
+  {
+    title: 'The Matrix',
+    director: 'Lana Wachowski, Lilly Wachowski'
+  },
+  {
+    title: 'Goodfellas',
+    director: 'Martin Scorsese'
+  }
+];
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to my movie club!');
+});
+
+app.get('/documentation', (req, res) => {                  
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
