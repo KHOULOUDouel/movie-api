528030b6d97f30bb6c47183ee0fcd10717bf2798
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-05-01 15:20:20 +0700

    Export the models from the "models.js" file using module.exports

--- models.js
+++ models.js
@@ -2,23 +2,23 @@ const mongoose = require('mongoose');
 
 // Define the Movies schema
 const movieSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    genre: { type: String, required: true },
-    director: { type: String, required: true },
-    releaseYear: { type: Number, required: true },
-    rating: { type: Number, required: true },
-    additionalAttributes: { type: Object }
+    title: { type: String, required: true }, // Title of the movie
+    genre: { type: String, required: true }, // Genre of the movie
+    director: { type: String, required: true }, // Director of the movie
+    releaseYear: { type: Number, required: true }, // Release year of the movie
+    rating: { type: Number, required: true }, // Rating of the movie
+    additionalAttributes: { type: Object } // Additional attributes of the movie
 });
 
 // Define the Users schema
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    email: { type: String, required: true },
-    favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+    username: { type: String, required: true }, // Username of the user
+    email: { type: String, required: true }, // Email of the user
+    favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }] // Array of favorite movies for the user
 });
 
 // Create models based on the schemas
-const Movie = mongoose.model('Movie', movieSchema);
-const User = mongoose.model('User', userSchema);
+const Movie = mongoose.model('Movie', movieSchema); // Movie model
+const User = mongoose.model('User', userSchema); // User model
 
-module.exports = { Movie, User };
+module.exports = { Movie, User }; // Export models
