eef0ddd28b4cc693fc452d3dc5a9a27e30eef05b
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-05-03 18:52:28 +0700

    Remove duplicate lines

--- models.js
+++ models.js
@@ -1,32 +1,26 @@
-const mongoose = require('mongoose');
-
 // Define the Movies schema
 const movieSchema = new mongoose.Schema({
-    title: { type: String, required: true }, // Title of the movie
-    genre: { type: String, required: true }, // Genre of the movie
-    director: { type: String, required: true }, // Director of the movie
-    releaseYear: { type: Number, required: true }, // Release year of the movie
-    rating: { type: Number, required: true }, // Rating of the movie
-    additionalAttributes: { type: Object } // Additional attributes of the movie
+  title: { type: String, required: true }, // Title of the movie
+  genre: { type: String, required: true }, // Genre of the movie
+  director: { type: String, required: true }, // Director of the movie
+  releaseYear: { type: Number, required: true }, // Release year of the movie
+  rating: { type: Number, required: true }, // Rating of the movie
+  additionalAttributes: { type: Object }, // Additional attributes of the movie
 });
 
 // Define the Users schema
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true }, // Username of the user
-    email: { type: String, required: true }, // Email of the user
-    favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }] // Array of favorite movies for the user
+  username: { type: String, required: true }, // Username of the user
+  email: { type: String, required: true }, // Email of the user
+  favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }], // Array of favorite movies for the user
 });
 
 // Create models based on the schemas
-const Movie = mongoose.model('Movie', movieSchema); // Movie model
-const User = mongoose.model('User', userSchema); // User model
+const Movie = mongoose.model("Movie", movieSchema); // Movie model
+const User = mongoose.model("User", userSchema); // User model
 
 module.exports = { Movie, User }; // Export models
 
-const mongoose = require('mongoose');
-
-// Importing Movie and User models from models.js
-const { Movie, User } = require('./models');
+const mongoose = require("mongoose");
 
 // Your application code here
-
