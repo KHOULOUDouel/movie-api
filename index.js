784e06e47f08a1757406ce35a1dff9c79fae644a
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-05-15 12:31:16 +0200

    Update Connection String

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 
 // Connect to MongoDB database
-mongoose.connect('mongodb://localhost:27017/movieDB', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Error connecting to MongoDB:', err));
 
