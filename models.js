1733a27da1eecc247f2886c6261871104b46dfe1
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-05-24 14:04:14 +0200

    Correct Endpoints

--- models.js
+++ models.js
@@ -2,19 +2,32 @@ const mongoose = require('mongoose');
 
 // Define the Movies schema
 const movieSchema = new mongoose.Schema({
-  title: { type: String, required: true }, // Title of the movie
-  genre: { type: String, required: true }, // Genre of the movie
-  director: { type: String, required: true }, // Director of the movie
-  releaseYear: { type: Number, required: true }, // Release year of the movie
-  rating: { type: Number, required: true }, // Rating of the movie
-  additionalAttributes: { type: Object }, // Additional attributes of the movie
+  Title: { type: String, required: true }, // Title of the movie
+  Description: { type: String, required: true }, // Description of the movie
+  // Genre of the movie
+  Genre: {
+    Name: String,
+    Description: String
+  },
+  // Director of the movie
+  Director: {
+    Name: String,
+    Bio: String,
+    Birth: String,
+    Death: String
+  },
+  // Link to the movie image
+  ImagePath: String,
+  Featured: Boolean,
 });
 
 // Define the Users schema
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true }, // Username of the user
-  email: { type: String, required: true }, // Email of the user
-  favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }], // Array of favorite movies for the user
+  Username: { type: String, required: true }, // Username of the user
+  Password: { type: String, required: true }, // Password of the user
+  Email: { type: String, required: true }, // Email of the user
+  Birthday: Date, // Birthday of the user
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }], // Array of favorite movies for the user
 });
 
 // Create models based on the schemas
