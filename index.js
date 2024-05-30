0a256a86e512f605e9d778d30fa1a5557f26440b
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-05-30 22:01:52 +0200

    Implement CORS into the app

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Import the cors module
 const models = require('./models'); // Importing Mongoose models
 
 const Movies = models.Movie;
@@ -9,12 +10,29 @@ const Users = models.User;
 const app = express();
 app.use(bodyParser.json());
 
+// CORS configuration allowing specific origins
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true); // Allow requests with no origin (like mobile apps or CURL requests)
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+// If you want to allow all origins, use the following code instead:
+// app.use(cors());
+
 // Connect to MongoDB database
 mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Error connecting to MongoDB:', err));
 
-    // Importing passport module and configuration
+// Importing passport module and configuration
 const passport = require('passport');
 require('./passport');
 
@@ -48,7 +66,7 @@ app.get('/movies/:Title', jwtAuth, async (req, res) => {
 
         // If the movie is found, return its data
         if (movie) {
-            return res.json(movie)
+            return res.json(movie);
         } else {
             // If the movie is not found, send a 404 status code and a message
             res.status(404).json({ message: 'Movie not found' });
@@ -69,9 +87,9 @@ app.get('/movies/genre/:Name', jwtAuth, async (req, res) => {
                 Name: movie.Genre.Name,
                 Description: movie.Genre.Description
             }
-            return res.json(genre)
+            return res.json(genre);
         } else {
-            res.status(404).json({ message: 'Genre not found' })
+            res.status(404).json({ message: 'Genre not found' });
         }
     } catch (error) {
         console.error(error);
@@ -95,11 +113,11 @@ app.get('/movies/director/:Name', jwtAuth, async (req, res) => {
             };
             return res.json(directorInfo);
         } else {
-            res.status(404).json({ message: 'Genre not found' })
+            res.status(404).json({ message: 'Director not found' });
         }
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: err.message });
+        res.status(500).json({ error: error.message });
     }
 });
 
@@ -117,12 +135,11 @@ app.get('/users', jwtAuth, async (req, res) => {
 app.post('/users', async (req, res) => {
     const { Username, Password, Email, Birthday } = req.body;
     try {
-
         const user = await Users.findOne({ Username });
 
         // Check if a user already exists
         if (user) {
-            return res.status(400).json({ message: Username + 'already exists' });
+            return res.status(400).json({ message: Username + ' already exists' });
         }
 
         const newUser = await Users.create({
@@ -130,7 +147,7 @@ app.post('/users', async (req, res) => {
             Password,
             Email,
             Birthday,
-        })
+        });
 
         res.status(201).json(newUser);
     } catch (err) {
@@ -153,13 +170,11 @@ app.get('/users/:Username', jwtAuth, async (req, res) => {
     }
 });
 
-
 // POST route to add a movie to user's favorites
 app.post('/users/:Username/movies/:MovieID', jwtAuth, async (req, res) => {
     const { Username, MovieID } = req.params;
 
     try {
-
         const updatedUser = await Users.findOneAndUpdate(
             { Username },
             { $push: { FavoriteMovies: MovieID } },
